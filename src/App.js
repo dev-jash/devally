4d673c35591c777d7ea0427d198e92372db899c5
dev-jash <dev.jash@outlook.com>
2024-09-24 01:38:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My React App!</h1>
+      <p>This is a simple React app deployed on GitHub.</p>
+    </div>
+  );
+}
+
+export default App;
